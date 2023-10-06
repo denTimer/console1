ee355b521bcb83782f98903a02936d91acd5d025
DenisNemirich <denTimer@yandex.ru>
2023-10-06 14:19:45 +0200

    Убрал лишние записи

--- console1/Program.cs
+++ console1/Program.cs
@@ -1,5 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("1 Hello, World!");
-Console.WriteLine("2 Hello, World!");
-Console.WriteLine("3 Hello, World!");
-Console.WriteLine("4 Hello, World!");
+﻿Console.WriteLine("1 Hello, World!");
