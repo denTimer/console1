74305f9da6a01320267745460c36ab400f3d5c86
DenisNemirich <denTimer@yandex.ru>
2023-10-06 14:18:35 +0200

    Console001

---
+++ console1/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("1 Hello, World!");
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("3 Hello, World!");
+Console.WriteLine("4 Hello, World!");
